a18ac9228d64560527cb812e425f0c19916146c2
syunk38 <syunk38@gmail.com>
2015-07-16 03:12:19 +0900

    save

--- routes/comments.js
+++ routes/comments.js
@@ -1,26 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
-// router.get('/', function(req, res){
-//   var data = [
-//     {author: "Pete Hunt", text: "This is one comment"},
-//     {author: "Jordan Walke", text: "This is *私の！* comment"}
-//   ];
-//   data.name = req.body.name;
-//   res.set('Content-Type', 'application/json');
-//   res.send(JSON.stringify(data));
-//   //res.json(data);
-// });
 
+var fs = require('fs');
 router.get('/', function(req, res) {
-  fs.readFile(__dirname + '/json/comments.json', function(err, data) {
+  fs.readFile(process.cwd() + '/json/comments.json', function(err, data) {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Content-Encoding', 'utf-8');
     res.setHeader('Cache-Control', 'no-cache');
-    res.send(JSON.stringify(data));
+    res.send(data);
   });
 });
-
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
 
 module.exports = router;
