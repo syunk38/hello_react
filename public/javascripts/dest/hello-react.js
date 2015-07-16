ed77a769b87029f115ad618bd57a977cb7a3d4e3
syunk38 <syunk38@gmail.com>
2015-07-16 23:32:54 +0900

    save

--- public/javascripts/dest/hello-react.js
+++ public/javascripts/dest/hello-react.js
@@ -1,13 +1,5 @@
-var data = [
-  {author: "Pete Hunt", text: "This is one comment"},
-  {author: "Jordan Walke", text: "This is *私の！* comment"}
-]
-
 var CommentBox = React.createClass({displayName: "CommentBox",
-  getInitialState: function() {
-    return {data: []};
-  },
-  componentDidMount: function() {
+  loadCommentsFromServer: function() {
     $.ajax({
       url: this.props.url,
       dataType: 'json',
@@ -20,11 +12,36 @@ var CommentBox = React.createClass({displayName: "CommentBox",
       }.bind(this)
     });
   },
+  handleCommentSubmit: function(comment) {
+    var comments = this.state.data;
+    var newComments = comments.concat([comment]);
+    this.setState({data: newComments});
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
   render: function() {
     return (
       React.createElement("div", {className: "commentBox"}, 
         React.createElement("h1", null, "Comments"), 
-        React.createElement(CommentList, {data: this.state.data})
+        React.createElement(CommentList, {data: this.state.data}), 
+        React.createElement(CommentForm, {onCommentSubmit: this.handleCommentSubmit})
       )
     );
   }
@@ -49,6 +66,30 @@ var CommentList = React.createClass({displayName: "CommentList",
   }
 });
 
+var CommentForm = React.createClass({displayName: "CommentForm",
+  handleSubmit: function(e) {
+    e.preventDefault(); // ブラウザデフォルトのサブミットをキャンセルしている。
+    var author = React.findDOMNode(this.refs.author).value.trim();
+    var text = React.findDOMNode(this.refs.text).value.trim();
+    if (!text || !author) {
+      return;
+    }
+    this.props.onCommentSubmit({author: author, text: text});
+    React.findDOMNode(this.refs.author).value = '';
+    React.findDOMNode(this.refs.text).value = '';
+    return;
+  },
+  render: function() {
+    return (
+      React.createElement("form", {className: "commentForm", onSubmit: this.handleSubmit}, 
+        React.createElement("input", {type: "text", placeholder: "Your name", ref: "author"}), 
+        React.createElement("input", {type: "text", placeholder: "Say something...", ref: "text"}), 
+        React.createElement("input", {type: "submit", value: "Post"})
+      )
+    );
+  }
+});
+
 var Comment = React.createClass({displayName: "Comment",
   render: function() {
     var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
@@ -65,7 +106,7 @@ var Comment = React.createClass({displayName: "Comment",
 
 window.onload = function() {
   React.render(
-      React.createElement(CommentBox, {url: "http://localhost:3000/comments"}),
+      React.createElement(CommentBox, {url: "http://localhost:3000/comments", pollInterval: ""}),
       document.getElementById('app-container')
   );
 }
--- routes/comments.js
+++ routes/comments.js
@@ -2,12 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 var fs = require('fs');
+var path = require('path');
+var jsonFilePath = process.cwd() + '/json/comments.json';
+
 router.get('/', function(req, res) {
   fs.readFile(process.cwd() + '/json/comments.json', function(err, data) {
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Content-Encoding', 'utf-8');
     res.setHeader('Cache-Control', 'no-cache');
-    res.send(data);
+    res.json(JSON.parse(data));
+    //res.send(data)
+  });
+});
+
+router.post('/', function(req, res) {
+  fs.readFile(jsonFilePath, function(err, data) {
+    var commentData = JSON.parse(data);
+    commentData.push(req.body);
+    fs.writeFile(jsonFilePath, JSON.stringify(commentData, null, 4), function(err) {
+      res.setHeader('Content-Type', 'application/json');
+      res.setHeader('Content-Encoding', 'utf-8');
+      res.setHeader('Cache-Control', 'no-cache');
+      res.json(commentData);
+    });
   });
 });
 
